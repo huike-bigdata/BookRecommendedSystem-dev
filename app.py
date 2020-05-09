2a57946bcd714f3b43c2a47c4a3a5f7daa3ad49c
imldy <im.ldy@outlook.com>
2020-05-09 14:58:59 +0800

    前后端初步结合，可以接收来自网页的评分

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, request
+from urllib.parse import parse_qs
 
 app = Flask(__name__)
 
@@ -12,16 +13,41 @@ def hello_world():
 @app.route("/Ratings")
 def Ratings():
     """
-    在这里，需要ISBN号、以及其封面、作者名、书名
+    在这里，需要ISBN号、以及其封面、作者名、书名(都是列表)
     :return:
     """
+    ISBNList = ['0886774632', '050552600X', '1558686274', '0440223202', '0312966776']
     res = make_response(render_template("Ratings.html",
-                                        Acccc=range(10),
-                                        ISBN="11111",
+                                        Acccc=range(len(ISBNList)),
+                                        ISBNList=ISBNList,
                                         fengMian=["http://images.amazon.com/images/P/0425176428.01.LZZZZZZZ.jpg"],
                                         Title="IF you",
                                         Author="AAAA"))
     return res
+
+
+@app.route("/submitinfo", methods=["GET", "POST"])
+def submitinfo():
+    if request.method == "GET":
+        res = make_response(render_template("Ratings.html"))
+        return res
+    elif request.method == "POST":
+        # print(type(request.files))
+        # print(request.files.items())
+        # print(request.files.keys())
+        # for i in request.files.keys():
+        #     print(i)
+        # res = request.data
+        # print("res：".format(res))
+        data = request.get_data()
+        print(data)
+        datadict = parse_qs(data)
+        print(datadict)
+        # data = json.loads(data)
+        # print(data)
+        return "成功了"
+    else:
+        return "error"
 
 
 if __name__ == '__main__':
