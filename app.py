a016db26a8d311e6d1c3b07c8aa4f841e459ddba
imldy <im.ldy@outlook.com>
2020-05-09 13:43:15 +0800

    初步的做了一个列表

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, make_response, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    res = make_response(render_template("index.html"))
+    return res
+
+
+@app.route("/Ratings")
+def Ratings():
+    """
+    在这里，需要ISBN号、以及其封面、作者名、书名
+    :return:
+    """
+    res = make_response(render_template("Ratings.html", ISBN="11111",
+                                        fengMian="http://images.amazon.com/images/P/0425176428.01.LZZZZZZZ.jpg",
+                                        Title="IF you",
+                                        Author="AAAA"))
+    return res
 
 
 if __name__ == '__main__':
